c0e454c33f8fe265d6090ae0b2bbf7014623f805
Sharon Kim <21800087@handong.edu>
2019-05-07 15:42:11 +0900

    Created Student, Course Changed HGUCoursePatternAnalyzer

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -1,28 +1,32 @@
 package edu.handong.analysis;
 
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+
 import edu.handong.analysis.datamodel.Course;
 import edu.handong.analysis.datamodel.Student;
 
 public class HGUCoursePatternAnalyzer {
 	
-	String[] lines = {	"1999-1, JC Nam, Java Programming",
-						"1999-2, JC Nam, Programming Language Theory",
-						"1999-1, JC Nam, Data Structures",
-						"2001-1, JC Nam, Database Systems",
-						"2018-1, SB Lim, Java Programming",
-						"2018-2, SB Lim, Programming Language Theory",
-						"2019-1, SB Lim, Data Structures",
-						"2019-1, SB Lim, Algorithm Analysis",
-						"2018-1, SJ Kim, Java Programming",
-						"2018-2, SJ Kim, Programming Language Theory",
-						"2019-1, SJ Kim, Logic Design",
-						"2019-1, SJ Kim, Algorithm Analysis",
-						};
+	private String[] lines = {	"1999-1, JC Nam, Java Programming",
+								"1999-2, JC Nam, Programming Language Theory",
+								"1999-1, JC Nam, Data Structures",
+								"2001-1, JC Nam, Database Systems",
+								"2018-1, SB Lim, Java Programming",
+								"2018-2, SB Lim, Programming Language Theory",
+								"2019-1, SB Lim, Data Structures",
+								"2019-1, SB Lim, Algorithm Analysis",
+								"2018-1, SJ Kim, Java Programming",
+								"2018-2, SJ Kim, Programming Language Theory",
+								"2019-1, SJ Kim, Logic Design",
+								"2019-1, SJ Kim, Algorithm Analysis",
+							};
 
-	int numOfStudents;
-	int numOfCourses;
-	Student[] students;
-	Course[] courses;
+	private int numOfStudents;
+	private int numOfCourses;
+	private Student[] students;
+	private Course[] courses;
 	
 	/**
 	 * This method runs our analysis logic to get the list of student and course names from lines.
@@ -54,11 +58,33 @@ public class HGUCoursePatternAnalyzer {
 	 * @return
 	 */
 	private Student[] initiateStudentArrayFromLines(String[] lines) {
-		
+
 		// TODO: implement this method
+
+		List<Student> list = new ArrayList<Student>();
+
+
+		for(int i = 0; i < lines.length; i++) {
+			String line = lines[i];
+			StringTokenizer st = new StringTokenizer(line, ",");
+			String hakki = st.nextToken();
+			String name = st.nextToken().trim();
+
+			boolean isExist = false;
+			for (int j=0; j<list.size(); j++) {
+				if (name.equals(list.get(j).getName())) {
+					isExist = true;
+				}
+			}
+
+			if (!isExist) {
+				list.add(new Student(name));
+			}
+			
+		}
 		
 		
-		return null;
+		return list.toArray(new Student[1]);
 	}
 
 	/**
@@ -68,10 +94,16 @@ public class HGUCoursePatternAnalyzer {
 	 * @return boolean
 	 */
 	private boolean studentExist(Student[] students, Student student) {
-		
-		// TODO: implement this method
 
-		return false;
+		// TODO: implement this method
+		boolean result = false;
+		
+		for (int i=0; i<students.length; i++) {
+			if (students[i].getName().equals(student.getName()))
+				result = true;
+		}
+		
+		return result;
 	}
 	
 	/**
@@ -82,10 +114,32 @@ public class HGUCoursePatternAnalyzer {
 	private Course[] initiateCourseArrayFromLines(String[] lines) {
 		
 		// TODO: implement this method
-		
-		return null;
-	}
+		List<Course> secondList = new ArrayList<Course>();
 
+		
+		for(int i = 0; i < lines.length; i++) {
+			String line = lines[i];
+			StringTokenizer st = new StringTokenizer(line, ",");
+			String hakki = st.nextToken();
+			String name = st.nextToken();
+			String courseName = st.nextToken().trim();
+
+			boolean isExist = false;
+			for (int j=0; j<secondList.size(); j++) {
+				if (courseName.equals(secondList.get(j).getCourseName())) {
+					isExist = true;
+				}
+			}
+
+			if (!isExist) {
+				secondList.add(new Course(courseName));
+			}
+			
+		}
+		
+		
+		return secondList.toArray(new Course[1]);
+	}
 	/**
 	 * This method check if there is the same name of the second argument in the array, courses.
 	 * @param courses
@@ -95,8 +149,15 @@ public class HGUCoursePatternAnalyzer {
 	private boolean courseExist(Course[] courses, Course course) {
 		
 		// TODO: implement this method
-
-		return false;
+		
+		boolean result = false;
+		
+		for (int i=0; i<courses.length; i++) {
+			if (courses[i].getCourseName().equals(course.getCourseName()))
+				result = true;
+		}
+		
+		return result;
 	}
 
 }
